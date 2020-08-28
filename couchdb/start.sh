9568990ff3d457789608d5ceb31ab6ae6ab8f927
stephenwu <stephenwu2020@protonmail.com>
2020-08-28 17:51:15 +0800

    couchdb

---
+++ couchdb/start.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+function up(){
+  docker-compose up -d
+}
+
+function down(){
+  docker-compose down
+}
+
+function clean(){
+  down
+  rm -rf data
+}
+
+function setup(){
+  curl localhost:5984
+  curl -X PUT http://admin:password@localhost:5984/_users
+  curl -X PUT http://admin:password@localhost:5984/_replicator
+  curl -X PUT http://admin:password@localhost:5984/_global_changes
+}
+
+function logs(){
+    docker logs -f couchdb
+}
+
+
+function help() {
+  echo "Usage: ./start.sh <cmd>"
+  echo "cmd:"
+  echo "  up"
+  echo "  setup"
+  echo "  down"
+  echo "  logs"
+  echo "  clean"
+}
+
+MODE="$1"
+case $MODE in 
+  up )
+    up 
+    ;;
+  setup )
+    setup
+    ;;
+  down )
+    down
+    ;;
+  logs )
+    logs
+    ;;
+  clean )
+    clean
+    ;;
+  * )
+    help
+    exit 1
+esac
