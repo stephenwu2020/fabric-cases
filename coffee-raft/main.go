c3a3b34628cd50c8f391852297132da57632e0ad
stephenwu <stephenwu2020@protonmail.com>
2020-08-30 20:10:17 +0800

    code

--- coffee-raft/main.go
+++ coffee-raft/main.go
@@ -23,23 +23,6 @@ func main() {
 	// Need some time to vote a leader
 	time.Sleep(5 * time.Second)
 
-	/*
-			log.Println("Start Hong's raft node")
-			raftNodeOfHong, err := createRaftNode(&nodes[1], false)
-			_ = raftNodeOfHong
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			log.Println("Join Hong's raft node")
-			//f := raftNodeOfHong.AddVoter(raft.ServerID(nodes[0].ID), raft.ServerAddress(nodes[0].Bind), 0, 0)
-			f := raftNodeOfMing.AddVoter(raft.ServerID(nodes[1].ID), raft.ServerAddress(nodes[1].Bind), 0, 0)
-			if f.Error() != nil {
-				log.Fatal(f.Error())
-			}
-		time.Sleep(5 * time.Second)
-
-	*/
 	// Interact with user
 	ReadInput(cluster)
 
